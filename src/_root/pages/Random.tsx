5c66de04312a86ce8f23cc0a511da6cda6f45555
Francesco <francescocarvelli25@gmail.com>
2023-11-16 23:02:31 +0100

    adjusted a few things in random page

--- src/_root/pages/Random.tsx
+++ src/_root/pages/Random.tsx
@@ -2,14 +2,15 @@ import { Flex, HStack, Text, Select, Button } from "@chakra-ui/react";
 import { useState } from "react";
 import MovieSelect from "@/components/shared/GenreSelect";
 import WatchCard from "@/components/shared/WatchCard";
-import { Movie } from "@/types/common";
+import { Movie, Multi, TV } from "@/types/common";
+
+const url = "https://api.themoviedb.org/3/discover/";
 
 const Random = () => {
   const [media_type, setType] = useState("movie");
   const [genre, setGenre] = useState("");
   const [isloadingGenres, setIsLoadingGenres] = useState(true);
-  const [watchCard, setWatchCard] = useState<Movie>();
-  const url = "https://api.themoviedb.org/3/discover/";
+  const [watchCard, setWatchCard] = useState<Multi>();
 
   function changeType(event: React.ChangeEvent<HTMLSelectElement>) {
     setType(event.currentTarget.value);
@@ -32,18 +33,18 @@ const Random = () => {
         );
         const json = await response.json();
 
-        const randomMovie = Math.floor(Math.random() * 20);
-        setWatchCard(json.results[randomMovie]);
+        const randomIdx = Math.floor(Math.random() * 20);
+        setWatchCard(json.results[randomIdx]);
       } catch (error) {
-        console.error(`Error fetching for random mocie:`, error);
+        console.error(`Error fetching in Random Page:`, error);
       }
     };
     fetching();
   }
 
   return (
-    <Flex w="100%" flex={1} direction="column" rowGap={5}>
-      <HStack columnGap="16px">
+    <Flex flex={1} direction="column" rowGap={20} align="center">
+      <HStack w="100%" columnGap="16px">
         <Text fontSize="32px" fontWeight="bold">
           Search a random
         </Text>
@@ -67,19 +68,13 @@ const Random = () => {
           Search
         </Button>
       </HStack>
-      <Flex w="100%" flex={1} justify={"center"} align={"center"}>
+      <Flex w="60%" flex={1} justify="center" align="center">
         {watchCard ? (
           <WatchCard
             id={watchCard.id}
             media_type={media_type}
-            title={
-              watchCard.name ? watchCard.name : (watchCard.title as string)
-            }
-            SpecImageURL={
-              watchCard.backdrop_path
-                ? watchCard.backdrop_path
-                : (watchCard.poster_path as string)
-            }
+            title={watchCard.name || watchCard.title}
+            SpecImageURL={watchCard.backdrop_path || watchCard.poster_path}
           />
         ) : (
           ""
