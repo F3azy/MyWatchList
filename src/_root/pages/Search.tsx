22a4e97006b04c197788f3b534574eb161b7273b
Francesco <francescocarvelli25@gmail.com>
2023-11-16 21:54:02 +0100

    changed the fetch functionality in search page with useFetch adjusted assings props

--- src/_root/pages/Search.tsx
+++ src/_root/pages/Search.tsx
@@ -8,39 +8,20 @@ import {
   Input,
 } from "@chakra-ui/react";
 import WatchCard from "@/components/shared/WatchCard";
-import { Movie } from "@/types/common";
+import { Multi } from "@/types/common";
 import { SearchIcon } from "@chakra-ui/icons";
+import useFetch from "@/hooks/useFetch";
 
 const Search = () => {
-  const url = "https://api.themoviedb.org/3/search/";
+  const url = "https://api.themoviedb.org/3/search/multi";
   const [title, setTitle] = useState("");
-  const [watchCards, setWatchCards] = useState<Movie[]>([]);
 
-  useEffect(() => {
-    fetch(
-      url +
-        "multi" +
-        `?api_key=${
-          import.meta.env.VITE_MOVIE_API_KEY
-        }&query=${title}&language=en-US`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((movie) => {
-        setWatchCards(
-          movie.results.filter(
-            (m: Movie) => m.media_type != "person" && m.poster_path != null
-          )
-        );
-      })
-      .catch((error) => {
-        console.error("Error fetching movies or series data:", error);
-      });
-  }, [title]);
+  const { data: watchCards } = useFetch<{ results: Multi[] }>(
+    url +
+      `?api_key=${
+        import.meta.env.VITE_MOVIE_API_KEY
+      }&query=${title}&language=en-US`
+  );
 
   function getTitle(event: FormEvent<HTMLInputElement>): void {
     setTitle(event.currentTarget.value);
@@ -66,18 +47,13 @@ const Search = () => {
         />
       </InputGroup>
       <Grid w="100%" templateColumns="repeat(8, 1fr)" gap={6}>
-        {watchCards.map((watchcard) => (
+        {watchCards?.results.map((watchcard) => (
           <GridItem w="100%" key={watchcard.id}>
             <WatchCard
-              isLink={true}
-              minH="200px"
-              givenWidth="100%"
               id={watchcard.id}
-              type={watchcard.media_type as string}
-              title={
-                watchcard?.name ? watchcard?.name : (watchcard?.title as string)
-              }
-              SpecImageURL={watchcard?.poster_path as string}
+              type={watchcard.media_type}
+              title={watchcard.name || watchcard.title}
+              SpecImageURL={watchcard.poster_path}
             />
           </GridItem>
         ))}
