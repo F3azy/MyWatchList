a964042516c50fbf9573d4f7d3b48aae06a5702a
Francesco <francescocarvelli25@gmail.com>
2023-12-06 22:09:06 +0100

    carousel go back to first page if page changes

--- src/components/shared/Carousel.tsx
+++ src/components/shared/Carousel.tsx
@@ -58,6 +58,10 @@ const Carousel = ({
     return () => clearTimeout(timer);
   }, [clicked]);
 
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [children]);
+
   useInterval(
     () => {
       changePage(ACTIONS.NEXT);
