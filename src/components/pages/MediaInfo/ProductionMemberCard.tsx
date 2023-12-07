099d75f72fd719ca86bde96a5f228bde435f93d6
Francesco <francescocarvelli25@gmail.com>
2023-12-07 17:09:07 +0100

    added skeleton for loading images of the cvast or crew memebers

--- src/components/pages/MediaInfo/ProductionMemberCard.tsx
+++ src/components/pages/MediaInfo/ProductionMemberCard.tsx
@@ -1,5 +1,6 @@
 import { MediaProductionMember } from "@/types/mediaInfo";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image as ChakraIMG, Skeleton, Text } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 const imageURL = "https://image.tmdb.org/t/p/original/";
 
@@ -8,7 +9,37 @@ const ProductionMemberCard = ({
 }: {
   member: MediaProductionMember;
 }) => {
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const decode = async () => {
+      try {
+        const image = new Image();
+        image.src = imageURL + member.profile_path;
+        await image.decode();
+
+        const timer = setTimeout(() => setIsLoading(false), 1000);
+        return () => clearTimeout(timer);
+      } catch (error) {
+        console.error("Error decoding image:", error);
+        const timer = setTimeout(() => setIsLoading(false), 1000);
+        return () => clearTimeout(timer);
+      }
+    };
+
+    decode();
+  }, [member]);
+
   return (
+    <Skeleton
+    h="full"
+    isLoaded={!isloading}
+    startColor="brand.primary"
+    endColor="brand.tertiary"
+    fadeDuration={4}
+    boxShadow="0px 20px 15px -10px black"
+  >
     <Box
       h="full"
       position="relative"
@@ -16,14 +47,14 @@ const ProductionMemberCard = ({
       role="group"
       cursor="pointer"
     >
-      <Image
+      <ChakraIMG
         w="100%"
         h="100%"
         transform="scale(1) translate(0, 0)"
         transition="transform 500ms ease 0s"
         _groupHover={{ transform: "scale(2) translate(20%, 10%)" }}
         src={imageURL + member.profile_path}
-        fallbackSrc='https://via.placeholder.com/150x200'
+        fallbackSrc="https://via.placeholder.com/150x200"
         alt={member.name}
       />
       <Box
@@ -51,6 +82,7 @@ const ProductionMemberCard = ({
         )}
       </Box>
     </Box>
+    </Skeleton>
   );
 };
 
