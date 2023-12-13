ff7ed76c11f766279945d3c74d123a987cc1341e
Francesco <francescocarvelli25@gmail.com>
2023-12-13 20:45:30 +0100

    made the home page responsive

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, useMediaQuery } from "@chakra-ui/react";
 import Carousel from "@/components/shared/Carousel";
 import CarouselItem from "@/components/shared/CarouselItem";
 import WatchCard from "@/components/shared/WatchCard";
@@ -15,9 +15,16 @@ const Home = () => {
     error,
   } = useMultipleFetch<MultiMedia>(HomeCarousels.urls);
 
+  const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
+  const [isSmallerThan768] = useMediaQuery("(max-width: 767px)");
+
   return (
-    <Flex direction="column" rowGap="28px">
-      <Grid templateColumns="repeat(6, 1fr)" gap={5}>
+    <Flex direction="column" rowGap={{ base: "16px", xl: "28px" }}>
+      <Grid
+        templateColumns={{ base: "repeat(3, 1fr)", xl: "repeat(6, 1fr)" }}
+        rowGap={{ base: 4, xl: 5 }}
+        columnGap={{ base: 3, xl: 5 }}
+      >
         {CollectionBoxes.map((collection) => (
           <GridItem key={collection.name}>
             <Collection
@@ -33,14 +40,19 @@ const Home = () => {
           key={idx}
           carouselTitle={HomeCarousels.titles[idx]}
           elementsTotal={carousel.results.length}
-          visibleElements={5}
+          visibleElements={isLargerThan1280 ? 5 : 3.5}
           isloading={loading}
+          isScrollable={!isLargerThan1280}
+          showScroll={false}
+          gap={isSmallerThan768 ? 10 : 20}
         >
           {carousel.results.map((watchcard) => (
             <CarouselItem key={watchcard.id}>
               <WatchCard
                 watchCard={watchcard}
-                media_type={watchcard.media_type || HomeCarousels.media_type[idx]}
+                media_type={
+                  watchcard.media_type || HomeCarousels.media_type[idx]
+                }
               />
             </CarouselItem>
           ))}
