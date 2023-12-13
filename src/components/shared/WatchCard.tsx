4a4374c3c2e9a01292b4db17a7e4bb902f6c8f94
Francesco <francescocarvelli25@gmail.com>
2023-12-13 01:36:21 +0100

    use backdrops with english titles if useBackdrop=true

--- src/components/shared/WatchCard.tsx
+++ src/components/shared/WatchCard.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Image as ChakraIMG, Link, Skeleton } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import { MultiMediaResult } from "@/types/common";
+import { MediaImages } from "@/types/common";
+import useFetch from "@/hooks/useFetch";
 
 const imageURL = "https://image.tmdb.org/t/p/original/";
 
@@ -15,6 +17,15 @@ const WatchCard = ({
   useBackdrop?: boolean;
 }) => {
   const [isloading, setIsLoading] = useState(true);
+
+  const urlImages = `https://api.themoviedb.org/3/${
+    media_type || watchCard.media_type
+  }/${watchCard.id}/images?api_key=${
+    import.meta.env.VITE_MOVIE_API_KEY
+  }&include_image_language=en`;
+
+  const { data: images, loading: loadingImages } =
+    useFetch<MediaImages>(urlImages);
 
   useEffect(() => {
     setIsLoading(true);
@@ -36,55 +47,96 @@ const WatchCard = ({
     decode();
   }, [watchCard]);
 
-  return (
-    <Skeleton
-      h="100%"
-      borderRadius="4px"
-      isLoaded={!isloading}
-      startColor="brand.primary"
-      endColor="brand.tertiary"
-      fadeDuration={4}
-      boxShadow="0px 20px 15px -10px black"
-      transition="transform 500ms ease 0s, box-shadow 500ms ease 0s"
-      _hover={{
-        transform: "translate(0, -10px)",
-        boxShadow: "0px 30px 30px -10px black",
-      }}
-    >
-      <Link
-        as={RouterLink}
-        to={
-          "/info/" +
-          (media_type || watchCard.media_type) +
-          "/" +
-          watchCard.id +
-          "/" +
-          (watchCard.title?.replaceAll(" ", "-") ||
-            watchCard.name?.replaceAll(" ", "-"))
-        }
+  if (useBackdrop)
+    return (
+      <Skeleton
+        h="100%"
+        borderRadius="4px"
+        isLoaded={!loadingImages}
+        startColor="brand.primary"
+        endColor="brand.tertiary"
+        fadeDuration={4}
+        boxShadow="0px 20px 15px -10px black"
+        transition="transform 500ms ease 0s, box-shadow 500ms ease 0s"
+        _hover={{
+          transform: "translate(0, -10px)",
+          boxShadow: "0px 30px 30px -10px black",
+        }}
       >
-        <ChakraIMG
-          h="100%"
-          borderRadius="8px"
-          border="4px solid"
-          borderColor="brand.dark.600"
-          background="linear-gradient(#141414 97%, #030303) border-box"
-          _hover={{
-            borderColor: "transparent",
-            background:
-              "linear-gradient(#141414, #141414) padding-box, linear-gradient(to right, #0B92F0, #0FF4C6) border-box",
-          }}
-          src={
-            imageURL +
-            (useBackdrop
-              ? watchCard.backdrop_path || watchCard.poster_path
-              : watchCard.poster_path)
+        <Link
+          as={RouterLink}
+          to={
+            "/info/" +
+            (media_type || watchCard.media_type) +
+            "/" +
+            watchCard.id +
+            "/" +
+            (watchCard.title?.replaceAll(" ", "-") ||
+              watchCard.name?.replaceAll(" ", "-"))
           }
-          alt={watchCard.title || watchCard.name}
-        />
-      </Link>
-    </Skeleton>
-  );
+        >
+          <ChakraIMG
+            h="100%"
+            borderRadius="8px"
+            border="4px solid"
+            borderColor="brand.dark.600"
+            background="linear-gradient(#141414 97%, #030303) border-box"
+            _hover={{
+              borderColor: "transparent",
+              background:
+                "linear-gradient(#141414, #141414) padding-box, linear-gradient(to right, #0B92F0, #0FF4C6) border-box",
+            }}
+            src={imageURL + images?.backdrops?.at(0)?.file_path}
+            alt={watchCard.title || watchCard.name}
+          />
+        </Link>
+      </Skeleton>
+    );
+  else
+    return (
+      <Skeleton
+        h="100%"
+        borderRadius="4px"
+        isLoaded={!isloading}
+        startColor="brand.primary"
+        endColor="brand.tertiary"
+        fadeDuration={4}
+        boxShadow="0px 20px 15px -10px black"
+        transition="transform 500ms ease 0s, box-shadow 500ms ease 0s"
+        _hover={{
+          transform: "translate(0, -10px)",
+          boxShadow: "0px 30px 30px -10px black",
+        }}
+      >
+        <Link
+          as={RouterLink}
+          to={
+            "/info/" +
+            (media_type || watchCard.media_type) +
+            "/" +
+            watchCard.id +
+            "/" +
+            (watchCard.title?.replaceAll(" ", "-") ||
+              watchCard.name?.replaceAll(" ", "-"))
+          }
+        >
+          <ChakraIMG
+            h="100%"
+            borderRadius="8px"
+            border="4px solid"
+            borderColor="brand.dark.600"
+            background="linear-gradient(#141414 97%, #030303) border-box"
+            _hover={{
+              borderColor: "transparent",
+              background:
+                "linear-gradient(#141414, #141414) padding-box, linear-gradient(to right, #0B92F0, #0FF4C6) border-box",
+            }}
+            src={imageURL + watchCard.poster_path}
+            alt={watchCard.title || watchCard.name}
+          />
+        </Link>
+      </Skeleton>
+    );
 };
 
 export default WatchCard;
