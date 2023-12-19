94778d88ee73a0a60b231fa00a479e0d3310cee1
Francesco <francescocarvelli25@gmail.com>
2023-12-19 17:11:24 +0100

    added important function to the AuthProvider and made the provider value object type

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,14 +1,71 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { auth } from "@/firebase";
+import {
+  User,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
 
-const AuthContext = React.createContext<null>(null);
+type AuthProviderValue = {
+  user: User | null;
+  signUp: (email: string, password: string) => void;
+  signIn: (email: string, password: string) => void;
+  logOut: () => void;
+  googleSignIn: () => void;
+};
+
+const AuthContext = React.createContext<AuthProviderValue | null>(null);
 
 export function useAuth() {
   return useContext(AuthContext);
 }
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return <AuthContext.Provider value={null}>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const signUp = (email: string, password: string) => {
+    createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email: string, password: string) => {
+    signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value: AuthProviderValue = {
+    user,
+    signUp,
+    signIn,
+    logOut,
+    googleSignIn,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
