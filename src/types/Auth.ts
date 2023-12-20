260e836e05fe00e974ac1a60620d85ea95412d25
Francesco <francescocarvelli25@gmail.com>
2023-12-20 20:57:06 +0100

    added types for new function and added more fucntion arg to the signUp function

--- src/types/Auth.ts
+++ src/types/Auth.ts
@@ -4,13 +4,19 @@ export type FirebaseProviders = "Google" | "Facebook" | "Twitter";
 
 export type FirebaseProvidersSignIn = {
   [key in FirebaseProviders]: () => Promise<UserCredential>;
-}
+};
 
 export type AuthProviderValue = {
   user: User | null;
-  signUp: (email: string, password: string) => Promise<UserCredential>;
+  signUp: (
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string
+  ) => Promise<void>;
   signIn: (email: string, password: string) => Promise<UserCredential>;
   resetEmail: (email: string) => Promise<void>;
+  resendEmailVerification: () => Promise<void> | undefined;
   logOut: () => Promise<void>;
   providersSignIn: FirebaseProvidersSignIn;
 };
