f9da1a575b80ddf69050915e8c4447d0a6418a7f
Francesco <francescocarvelli25@gmail.com>
2023-12-20 03:29:01 +0100

    added the sign in with socials logic for now only google work, problems with facebook and twitter for developers

--- src/components/auth/AuthIcons.tsx
+++ src/components/auth/AuthIcons.tsx
@@ -1,16 +1,41 @@
-import { Button, ButtonGroup, Icon, Text } from "@chakra-ui/react";
+import { Button, Icon, Text, VStack } from "@chakra-ui/react";
 import { Icons } from "@/constans/AuthIcons";
+import { useAuth } from "@/contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { FirebaseProviders } from "@/types/Auth";
 
 const AuthIcons = () => {
+  const navigate = useNavigate();
+
+  const context = useAuth();
+
+  if (!context) return null;
+
+  const { providersSignIn } = context;
+
+  const handleProviderSignIn = async (provider: FirebaseProviders) => {
+    try {
+      await providersSignIn[provider]();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <ButtonGroup variant="outline" spacing="4" width="full">
+    <VStack spacing="4" width="full">
       {Icons.map(({ name, icon }) => (
-        <Button key={name} width="full">
+        <Button
+          key={name}
+          width="full"
+          variant="outline"
+          onClick={() => handleProviderSignIn(name)}
+        >
           <Icon as={icon} boxSize={5} />
           <Text ml={2}>Sign in with {name}</Text>
         </Button>
       ))}
-    </ButtonGroup>
+    </VStack>
   );
 };
 
--- src/constans/AuthIcons.ts
+++ src/constans/AuthIcons.ts
@@ -1,6 +1,8 @@
+import { FirebaseProviders } from "@/types/Auth";
+import { IconType } from "react-icons";
 import { BsGoogle, BsTwitter, BsFacebook } from "react-icons/bs";
 
-export const Icons = [
+export const Icons: { name: FirebaseProviders; icon: IconType }[] = [
   {
     name: "Google",
     icon: BsGoogle,
