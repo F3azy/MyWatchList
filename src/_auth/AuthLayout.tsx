fd2c7f393c3ad1bff76475e5f3e3b29ec6dab63f
Francesco <francescocarvelli25@gmail.com>
2023-12-20 20:47:52 +0100

    changed the h to minH in AuthLayout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -2,12 +2,12 @@ import { Flex, Container } from "@chakra-ui/react";
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <Flex h="100vh" align="center">
+    <Flex minH="100vh" align="center">
       <Container
         display={{ base: "flex", md: "block" }}
         alignItems="center"
         w={{ base: "100%", xl: "25%" }}
-        h={{ base: "full", md: "auto" }}
+        minH={{ base: "100vh", md: "auto" }}
         borderRadius={{ base: "none", md: "16px" }}
         p="16px"
         variant="gradient-with-shadow"
