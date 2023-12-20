53a56d4a27b5d220583aaf642b9f7bcecfe06ae3
Francesco <francescocarvelli25@gmail.com>
2023-12-20 03:26:13 +0100

    adjusted the ui for mobile view and added the sing in/up ligic with firebase

--- src/_auth/forms/SignInForm.tsx
+++ src/_auth/forms/SignInForm.tsx
@@ -1,5 +1,7 @@
-import { FormControl, FormLabel, Heading } from "@chakra-ui/react";
 import {
+  FormControl,
+  FormLabel,
+  Heading,
   Input,
   Divider,
   Button,
@@ -10,60 +12,109 @@ import {
 } from "@chakra-ui/react";
 import PasswordField from "@/components/forms/PasswordField";
 import AuthIcons from "@/components/auth/AuthIcons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FormEvent, useRef, useState } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import AuthLayout from "../AuthLayout";
 
 const SignInForm = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passRef = useRef<HTMLInputElement>(null);
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const context = useAuth();
+
+  if (!context) return null;
+
+  const { signIn } = context;
+
+  async function handleSubmit(event: FormEvent<HTMLDivElement>) {
+    event.preventDefault();
+
+    if (emailRef.current?.value === "") {
+      setError("Email is required.");
+      return;
+    }
+
+    if (passRef.current?.value === "") {
+      setError("Passwords is required");
+      return;
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signIn(
+        emailRef.current?.value as string,
+        passRef.current?.value as string
+      );
+      navigate("/");
+    } catch (error) {
+      setError("Failed to sign in to account");
+    }
+
+    setLoading(false);
+  }
+
   return (
-    <Stack w="full" spacing={5}>
-      <Heading textAlign="center" color="brand.secondary">
-        Sign In
-      </Heading>
-      <VStack as="form" w="full" spacing={5}>
-        <FormControl>
-          <FormLabel color={"brand.secondary"}>Email</FormLabel>
-          <Input
-            id="email"
-            type="email"
-            placeholder="example@domain.com"
+    <AuthLayout>
+      <Stack w="full" spacing={5}>
+        <Heading textAlign="center" color="brand.secondary">
+          Sign In
+        </Heading>
+        <VStack as="form" w="full" spacing={5} onSubmit={handleSubmit}>
+          {error !== "" && <Text color="red.500">{error}</Text>}
+          <FormControl isRequired>
+            <FormLabel color={"brand.secondary"}>Email</FormLabel>
+            <Input
+              ref={emailRef}
+              id="email"
+              type="email"
+              placeholder="example@domain.com"
+              color="brand.secondary"
+              borderColor="brand.secondary"
+              _hover={{ borderColor: "brand.primary" }}
+            />
+          </FormControl>
+          <PasswordField passRef={passRef} />
+          <Button w="full" variant="full" type="submit" isLoading={loading}>
+            Sign in
+          </Button>
+        </VStack>
+        <HStack justify="space-between">
+          <Text
+            as={Link}
+            to="/signup"
+            textAlign="center"
             color="brand.secondary"
-            borderColor="brand.secondary"
-            _hover={{ borderColor: "brand.primary" }}
-          />
-        </FormControl>
-        <PasswordField />
-        <Button w="full" variant="full">
-          Sign in
-        </Button>
-      </VStack>
-      <HStack justify="space-between">
-        <Text
-          as={Link}
-          to="/signup"
-          textAlign="center"
-          color="brand.secondary"
-          textDecoration="underline"
-        >
-          Don't have an account?
-        </Text>
-        <Text
-          as={Link}
-          to="/reset-password"
-          textAlign="center"
-          color="brand.secondary"
-          textDecoration="underline"
-        >
-          Forgot Password?
-        </Text>
-      </HStack>
-      <HStack>
-        <Divider />
-        <Text fontSize="sm" whiteSpace="nowrap">
-          OR
-        </Text>
-        <Divider />
-      </HStack>
-      <AuthIcons />
-    </Stack>
+            textDecoration="underline"
+          >
+            Don't have an account?
+          </Text>
+          <Text
+            as={Link}
+            to="/reset-password"
+            textAlign="center"
+            color="brand.secondary"
+            textDecoration="underline"
+          >
+            Forgot Password?
+          </Text>
+        </HStack>
+        <HStack>
+          <Divider />
+          <Text fontSize="sm" whiteSpace="nowrap">
+            OR
+          </Text>
+          <Divider />
+        </HStack>
+        <AuthIcons />
+      </Stack>
+    </AuthLayout>
   );
 };
 
--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,53 +1,122 @@
-import { FormControl, FormLabel, Heading, VStack } from "@chakra-ui/react";
-import { Input, Divider, Button, Text, Stack, HStack } from "@chakra-ui/react";
+import {
+  Input,
+  Divider,
+  Button,
+  Text,
+  Stack,
+  HStack,
+  FormControl,
+  FormLabel,
+  Heading,
+  VStack,
+} from "@chakra-ui/react";
 import PasswordField from "@/components/forms/PasswordField";
 import AuthIcons from "@/components/auth/AuthIcons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "@/contexts/AuthContext";
+import { FormEvent, useRef, useState } from "react";
+import AuthLayout from "../AuthLayout";
 
 const SignUpForm = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passRef = useRef<HTMLInputElement>(null);
+  const repPassRef = useRef<HTMLInputElement>(null);
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const context = useAuth();
+
+  if (!context) return null;
+
+  const { signUp } = context;
+
+  async function handleSubmit(event: FormEvent<HTMLDivElement>) {
+    event.preventDefault();
+
+    if (emailRef.current?.value === "") {
+      setError("Email is required.");
+      return;
+    }
+
+    if (passRef.current?.value === "" || repPassRef.current?.value === "") {
+      setError("Passwords is required");
+      return;
+    }
+
+    if (passRef.current?.value.length && passRef.current?.value.length < 6) {
+      setError("Password must have at least 6 characters");
+      return;
+    }
+
+    if (passRef.current?.value !== repPassRef.current?.value) {
+      setError("Passwords are not the same");
+      return;
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signUp(
+        emailRef.current?.value as string,
+        passRef.current?.value as string
+      );
+      navigate("/");
+    } catch {
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
+  }
+
   return (
-    <Stack w="full" spacing={5}>
-      <Heading textAlign="center" color="brand.secondary">
-        Sign Up
-      </Heading>
-      <VStack as="form" w="full" spacing={5}>
-        <FormControl>
-          <FormLabel color={"brand.secondary"}>Email</FormLabel>
-          <Input
-            id="email"
-            type="email"
-            placeholder="example@domain.com"
-            pattern=".+@example\.com"
-            color="brand.secondary"
-            borderColor="brand.secondary"
-            _hover={{ borderColor: "brand.primary" }}
-          />
-        </FormControl>
-        <PasswordField />
-        <PasswordField repeat />
-        <Button w="full" variant="full">
-          Sign up
-        </Button>
-      </VStack>
-      <Text
-        as={Link}
-        to="/signin"
-        textAlign="center"
-        color="brand.secondary"
-        textDecoration="underline"
-        mx="auto"
-      >
-        Already have an account?
-      </Text>
-      <HStack>
-        <Divider />
-        <Text fontSize="sm" whiteSpace="nowrap">
-          OR
+    <AuthLayout>
+      <Stack w="full" spacing={5}>
+        <Heading textAlign="center" color="brand.secondary">
+          Sign Up
+        </Heading>
+        <VStack as="form" w="full" spacing={5} onSubmit={handleSubmit}>
+          {error !== "" && <Text color="red.500">{error}</Text>}
+          <FormControl isRequired>
+            <FormLabel color={"brand.secondary"}>Email</FormLabel>
+            <Input
+              ref={emailRef}
+              id="email"
+              type="email"
+              placeholder="example@domain.com"
+              color="brand.secondary"
+              borderColor="brand.secondary"
+              _hover={{ borderColor: "brand.primary" }}
+            />
+          </FormControl>
+          <PasswordField passRef={passRef} />
+          <PasswordField repeat passRef={repPassRef} />
+          <Button w="full" variant="full" type="submit" isLoading={loading}>
+            Sign up
+          </Button>
+        </VStack>
+        <Text
+          as={Link}
+          to="/signin"
+          textAlign="center"
+          color="brand.secondary"
+          textDecoration="underline"
+          mx="auto"
+        >
+          Already have an account?
         </Text>
-        <Divider />
-      </HStack>
-      <AuthIcons />
-    </Stack>
+        <HStack>
+          <Divider />
+          <Text fontSize="sm" whiteSpace="nowrap">
+            OR
+          </Text>
+          <Divider />
+        </HStack>
+        <AuthIcons />
+      </Stack>
+    </AuthLayout>
   );
 };
 
