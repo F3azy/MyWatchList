e48ac8d5b9fd79f20afc8349b9230102b85b2951
Francesco <francescocarvelli25@gmail.com>
2023-12-21 14:18:00 +0100

    changed maxW of container in AuthLayout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,12 +1,12 @@
 import { Flex, Container } from "@chakra-ui/react";
 
-const AuthLayout = ({children}: {children: React.ReactNode}) => {
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Flex minH="100vh" align="center">
       <Container
         display={{ base: "flex", md: "block" }}
-        alignItems="center"
-        w={{ base: "100%", xl: "25%" }}
+        alignItems={{ base: "center", md: "" }}
+        maxW={{base: "full", md: "55%", xl: "40%", '2xl': "30%"}}
         minH={{ base: "100vh", md: "auto" }}
         borderRadius={{ base: "none", md: "16px" }}
         p="16px"
