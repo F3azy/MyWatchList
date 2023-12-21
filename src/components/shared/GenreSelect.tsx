d3de5bcc50a5b0ada80322cf7697bd26f9dc2b1b
Francesco <francescocarvelli25@gmail.com>
2023-12-21 15:18:03 +0100

    refactorrd ghenreSelect compoennt

--- src/components/shared/GenreSelect.tsx
+++ src/components/shared/GenreSelect.tsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Select, Skeleton } from "@chakra-ui/react";
+import useFetch from "@/hooks/useFetch";
 
 type Genres = {
-  id: number;
-  name: string;
+  genres: {
+    id: number;
+    name: string;
+  }[];
 };
 
 const url = "https://api.themoviedb.org/3/genre/";
@@ -19,50 +22,28 @@ const GenreSelect = ({
   changeFun?: React.ChangeEventHandler<HTMLSelectElement>;
   defaultValue?: string;
 }) => {
-  const [genres, setGenres] = useState<Genres[]>([]);
-  const [isloading, setIsLoading] = useState(true);
+  const { data: genre, loading } = useFetch<Genres>(
+    url + media_type + `/list?api_key=${import.meta.env.VITE_MOVIE_API_KEY}`
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    const fetching = async () => {
-      try {
-        const response = await fetch(
-          url +
-            (media_type === "tv" ? "tv" : "movie") +
-            `/list?api_key=${import.meta.env.VITE_MOVIE_API_KEY}`
-        );
-        const json = await response.json();
-        setGenres(json.genres);
-        if (defaultValue !== "") setStateFun(defaultValue as string);
-        else setStateFun(json.genres[0].id);
-        setTimeout(() => setIsLoading(false), 800);
-      } catch (error) {
-        console.error("Error fetching genres:", error);
-        const timer = setTimeout(() => setIsLoading(false), 1200);
-        return () => clearTimeout(timer);
-      }
-    };
-
-    fetching();
+    if (defaultValue !== "") setStateFun(defaultValue as string);
+    else setStateFun(genre?.genres?.at(0)?.id.toString() as string);
   }, [media_type]);
 
   return (
     <Skeleton
       w={{ base: "full", md: "200px" }}
       borderRadius="md"
-      isLoaded={!isloading}
+      isLoaded={!loading}
       startColor="brand.primary"
       endColor="brand.tertiary"
       fadeDuration={3}
     >
-      <Select
-        variant="base"
-        defaultValue={defaultValue}
-        onChange={changeFun}
-      >
-        {genres?.map((genre) => (
-          <option key={genre.id} value={genre.id}>
-            {genre.name}
+      <Select variant="base" defaultValue={defaultValue} onChange={changeFun}>
+        {genre?.genres?.map((genre) => (
+          <option key={genre?.id} value={genre?.id}>
+            {genre?.name}
           </option>
         ))}
       </Select>
