6f398a6a7cbe919c17b83f900237114fa62c4c97
Francesco <francescocarvelli25@gmail.com>
2023-12-22 15:31:12 +0100

    changed one error message

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -6,14 +6,14 @@ export function handleErrors(error: FirebaseError): string {
       return "Provided email address already in use.";
     case "auth/invalid-email":
       return "Provided email address is invalid.";
-      case "auth/invalid-credential":
-        return "Email or password are incorrect.";
-      case "auth/weak-password":
-        return "Password must be at least 6 characters.";
+    case "auth/invalid-credential":
+      return "Email or password are incorrect.";
+    case "auth/weak-password":
+      return "Password must be at least 6 characters.";
     case "auth/operation-not-allowed":
       return "Error during email resend.";
     case "auth/too-many-requests":
-      return "Too many request, wait at least a minute and try again.";
+      return "Too many request, please wait a few seconds and try again.";
     default:
       return error.message;
   }
