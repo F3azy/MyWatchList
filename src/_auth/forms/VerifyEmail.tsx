46f2ab5b303eb91474a3dc8b2e30d47bc699653b
Francesco <francescocarvelli25@gmail.com>
2023-12-22 16:59:17 +0100

    handling user token expired error

--- src/_auth/forms/VerifyEmail.tsx
+++ src/_auth/forms/VerifyEmail.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import AuthLayout from "../AuthLayout";
-import { Button, Heading, Stack, Text } from "@chakra-ui/react";
+import { Button, Heading, Stack, Text, Link } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 import { FirebaseError } from "firebase/app";
 import { handleErrors } from "@/utils/firebase";
+import { Link as RouterLink } from "react-router-dom";
 
 const VerifyEmail = () => {
   const navigate = useNavigate();
 
   const [error, setError] = useState("");
+  const [tokenExpired, setTokenExpired] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const [seconds, setSeconds] = useState(59);
@@ -26,19 +28,30 @@ const VerifyEmail = () => {
       setLoading(true);
       await resendEmailVerification();
     } catch (error: unknown) {
-      if (error instanceof FirebaseError) setError(handleErrors(error));
+      if (error instanceof FirebaseError) {
+        console.log(error.code);
+
+        setError(handleErrors(error));
+      }
     }
 
     setLoading(false);
   }
 
   useEffect(() => {
-    const checkForVerifiedInterval = setInterval(() => {
-      user?.reload().then((ok) => {
+    const checkForVerifiedInterval = setInterval(async () => {
+      try {
+        await user?.reload();
         if (user?.emailVerified) {
           navigate("/");
         }
-      });
+      } catch (error) {
+        if (error instanceof FirebaseError) {
+          if (error.code === "auth/user-token-expired") setTokenExpired(true);
+          setError(handleErrors(error));
+          clearInterval(checkForVerifiedInterval);
+        }
+      }
     }, 2000);
 
     return () => clearInterval(checkForVerifiedInterval);
@@ -62,37 +75,53 @@ const VerifyEmail = () => {
 
   return (
     <AuthLayout>
-      <Stack w="full" spacing={8} textAlign="center" letterSpacing={1}>
-        {error !== "" && <Text color="red.500">{error}</Text>}
-        <Heading color="brand.secondary">Verify Email</Heading>
-        <Text>
-          You're almost there! We sent you and email to{" "}
-          <Text fontWeight="semibold" as="span">
-            {user?.email}
+      {tokenExpired ? (
+        <Stack w="full" spacing={8} textAlign="center" letterSpacing={1}>
+          <Heading color="red.500">Something went wrong.</Heading>
+          {error !== "" && <Text color="red.500">{error}</Text>}
+          <Link
+            as={RouterLink}
+            to="/signup"
+            color="brand.secondary"
+            textDecoration="underline"
+            fontSize="24px"
+          >
+            Go back to the signup form.
+          </Link>
+        </Stack>
+      ) : (
+        <Stack w="full" spacing={8} textAlign="center" letterSpacing={1}>
+          {error !== "" && <Text color="red.500">{error}</Text>}
+          <Heading color="brand.secondary">Verify Email</Heading>
+          <Text>
+            You're almost there! We sent you and email to{" "}
+            <Text fontWeight="semibold" as="span">
+              {user?.email}
+            </Text>
           </Text>
-        </Text>
-        <Text>
-          Just click on the link in that email to complete your singup. If you
-          don't see it, you may need to check your spam folder.
-        </Text>
-        <Text>Still can't find the email?</Text>
-        <Button
-          w="full"
-          variant="full"
-          onClick={
-            seconds > 0
-              ? () => {}
-              : () => {
-                  handleResend();
-                  resendTimer();
-                }
-          }
-          isLoading={loading}
-          isDisabled={seconds > 0}
-        >
-          {seconds > 0 ? seconds + "s" : "Resend Email"}
-        </Button>
-      </Stack>
+          <Text>
+            Just click on the link in that email to complete your singup. If you
+            don't see it, you may need to check your spam folder.
+          </Text>
+          <Text>Still can't find the email?</Text>
+          <Button
+            w="full"
+            variant="full"
+            onClick={
+              seconds > 0
+                ? () => {}
+                : () => {
+                    handleResend();
+                    resendTimer();
+                  }
+            }
+            isLoading={loading}
+            isDisabled={seconds > 0}
+          >
+            {seconds > 0 ? seconds + "s" : "Resend Email"}
+          </Button>
+        </Stack>
+      )}
     </AuthLayout>
   );
 };
